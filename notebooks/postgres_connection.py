ef30e5ac9fccc4a67afdfd13bdc62732d467b91e
Jason Reyes <reyesjason21@outlook.com>
2024-12-22 16:04:49 -0500

    Notebooks: Configured Pandas display settings.

--- notebooks/postgres_connection.py
+++ notebooks/postgres_connection.py
@@ -1,12 +1,19 @@
 from dotenv import load_dotenv
 from pathlib import Path
 import os
+import pandas as pd
 import psycopg
 
-# Get the absolute path to the .env file
+# Configure pandas display settings:
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.min_rows', 10)
+pd.set_option('display.expand_frame_repr', True)
+
+# Get the path to the .env file:
 dotenv_path = Path(__file__).parent.parent / '.env'
 load_dotenv(dotenv_path = dotenv_path)
 
+# PostgreSQL database connection:
 def db():
     return psycopg.connect(
         host = os.getenv('DB_HOST'),
