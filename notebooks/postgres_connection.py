6b2092bc7acf3dce03f99d131b3ec12acd7fb235
Jason Reyes <reyesjason21@outlook.com>
2025-01-08 21:23:53 -0500

    Notebooks: Refactored postgres_connection.py db() function to include error handling.

--- notebooks/postgres_connection.py
+++ notebooks/postgres_connection.py
@@ -1,8 +1,10 @@
-from dotenv import load_dotenv
-from pathlib import Path
 import os
+from pathlib import Path
+
 import pandas as pd
 import psycopg
+from psycopg import OperationalError, ProgrammingError
+from dotenv import load_dotenv
 
 # Configure pandas display settings:
 pd.set_option('display.max_rows', 50)
@@ -15,10 +17,20 @@ load_dotenv(dotenv_path = dotenv_path)
 
 # PostgreSQL database connection:
 def db():
-    return psycopg.connect(
-        host = os.getenv('DB_HOST'),
-        port = os.getenv('DB_PORT'),
-        user = os.getenv('DB_USER'),
-        password = os.getenv('DB_PASSWORD'),
-        dbname = os.getenv('DB_NAME')
-    )
+    try:
+        return psycopg.connect(
+            host = os.getenv('DB_HOST'),
+            port = os.getenv('DB_PORT'),
+            user = os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            dbname = os.getenv('DB_NAME')
+        )
+    except OperationalError as e:
+        print(f"Unable to connect to the database: {e}")
+        raise
+    except ProgrammingError as e:
+        print(f"An error occurred with the execution of the query: {e}")
+        raise
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        raise
